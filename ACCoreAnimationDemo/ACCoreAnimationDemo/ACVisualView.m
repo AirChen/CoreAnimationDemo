e87556f066d46c558b312e9595b995fa1d90ce41
AirChen <renzhichen2012@163.com>
2017-03-11 10:33:14 +0800

    shadow visual

--- ACCoreAnimationDemo/ACCoreAnimationDemo/ACVisualView.m
+++ ACCoreAnimationDemo/ACCoreAnimationDemo/ACVisualView.m
@@ -10,9 +10,6 @@
 
 @interface ACVisualView()
 
-@property (nonatomic, readwrite, strong) UIView *whiteView;
-@property (nonatomic, readwrite, strong) UIView *blueView;
-
 @end
 
 @implementation ACVisualView
@@ -21,7 +18,7 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        [self prepareDemo];
+        [self prepareShadowPath];
     }
     return self;
 }
@@ -35,30 +32,94 @@
     CGPoint suggestOrigan = CGPointMake(1/3.0*oriSize.width, 1/3.0*oriSize.height);
     CGSize suggestSize = CGSizeMake(2/3.0*oriSize.width, 2/3.0*oriSize.height);
     
-    _whiteView = [[UIView alloc] initWithFrame:CGRectMake(suggestOrigan.x, suggestOrigan.y, suggestSize.width, suggestSize.height)];
-    _blueView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, suggestSize.width, suggestSize.height)];
+    UIView *whiteView = [[UIView alloc] initWithFrame:CGRectMake(suggestOrigan.x, suggestOrigan.y, suggestSize.width, suggestSize.height)];
+    UIView *blueView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, suggestSize.width, suggestSize.height)];
     
-    _whiteView.backgroundColor = [UIColor whiteColor];
-    _blueView.backgroundColor = [UIColor blueColor];
+    whiteView.backgroundColor = [UIColor whiteColor];
+    blueView.backgroundColor = [UIColor blueColor];
     
-    [self addSubview:_whiteView];
-    [self addSubview:_blueView];
+    [self addSubview:whiteView];
+    [self addSubview:blueView];
     
     //corner
-    _whiteView.layer.contents = (__bridge id)[UIImage imageNamed:@"Unknown"].CGImage;
-    _whiteView.layer.contentsGravity = kCAGravityResizeAspect;
-    _whiteView.layer.cornerRadius = 5.0f;
-    _whiteView.layer.masksToBounds = YES;
+    whiteView.layer.contents = (__bridge id)[UIImage imageNamed:@"Unknown"].CGImage;
+    whiteView.layer.contentsGravity = kCAGravityResizeAspect;
+    whiteView.layer.cornerRadius = 5.0f;
+    whiteView.layer.masksToBounds = YES;
     
     self.layer.cornerRadius = 10.0f;
     self.layer.masksToBounds = YES;
     
     //border
-    _whiteView.layer.borderColor = [UIColor blackColor].CGColor;
-    _whiteView.layer.borderWidth = 2.0f;
+    whiteView.layer.borderColor = [UIColor blackColor].CGColor;
+    whiteView.layer.borderWidth = 2.0f;
     
     self.layer.borderColor = [UIColor blackColor].CGColor;
-    self.layer.borderWidth = 5.0f;
+    self.layer.borderWidth = 5.0f;//发现外层加边之后，内部视图的圆角效果没有了!!!
+    
+    //shadow
+    self.layer.shadowOpacity = 0.5f;//这里是没有阴影效果的
+    self.layer.shadowOffset = CGSizeMake(0.0f, 5.0f);
+    self.layer.shadowRadius = 5.0f;
+}
+
+//添加一个视图层专门负责阴影效果，相邻内部的一层用masksToBounds，就不会隔离阴影了
+- (void)prepareShadowDemo{
+    
+    CGSize oriSize = self.bounds.size;
+    
+    CGPoint suggestOrigan = CGPointMake(1/3.0*oriSize.width, 1/3.0*oriSize.height);
+    CGSize suggestSize = CGSizeMake(2/3.0*oriSize.width, 2/3.0*oriSize.height);
+    
+    UIView *whiteView = [[UIView alloc] initWithFrame:CGRectMake(suggestOrigan.x, suggestOrigan.y, suggestSize.width, suggestSize.height)];
+    UIView *blueView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, suggestSize.width, suggestSize.height)];
+    UIView *containerView = [[UIView alloc] initWithFrame:self.bounds];
+    
+    whiteView.backgroundColor = [UIColor whiteColor];
+    blueView.backgroundColor = [UIColor blueColor];
+    containerView.backgroundColor = [UIColor redColor];
+    
+    [containerView addSubview:whiteView];
+    [containerView addSubview:blueView];
+    [self addSubview:containerView];
+    
+    //corner
+    whiteView.layer.contents = (__bridge id)[UIImage imageNamed:@"Unknown"].CGImage;
+    whiteView.layer.contentsGravity = kCAGravityResizeAspect;
+    whiteView.layer.cornerRadius = 5.0f;
+    whiteView.layer.masksToBounds = YES;
+    
+    containerView.layer.cornerRadius = 10.0f;
+    containerView.layer.masksToBounds = YES;
+    
+    //border
+    whiteView.layer.borderColor = [UIColor blackColor].CGColor;
+    whiteView.layer.borderWidth = 2.0f;
+    
+    containerView.layer.borderColor = [UIColor blackColor].CGColor;
+    containerView.layer.borderWidth = 5.0f;//发现外层加边之后，内部视图的圆角效果没有了!!!
+    
+    //shadow
+    self.layer.shadowOpacity = 0.5f;//这里是没有阴影效果的
+    self.layer.shadowOffset = CGSizeMake(0.0f, 5.0f);
+    self.layer.shadowRadius = 5.0f;
+}
+
+- (void)prepareShadowPath{
+
+    self.layer.contents = (__bridge id)[UIImage imageNamed:@"dog"].CGImage;
+    self.layer.contentsGravity = kCAGravityResizeAspect;
+    self.layer.shadowOpacity = 0.5f;
+    
+//    CGMutablePathRef squrePath = CGPathCreateMutable();
+//    CGPathAddRect(squrePath, NULL, self.bounds);
+//    self.layer.shadowPath = squrePath;
+//    CGPathRelease(squrePath);
+    
+    CGMutablePathRef circlePath = CGPathCreateMutable();
+    CGPathAddEllipseInRect(circlePath, NULL, self.bounds);
+    self.layer.shadowPath = circlePath;
+    CGPathRelease(circlePath);
     
 }
 
